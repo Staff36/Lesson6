7356312d5fc5d7338ee62535bee514fb8275b892
TroninDmitry <dima-tronin@mail.ru>
2021-01-13 23:21:50 +0300

    Сделал ДЗ

--- src/com/Lesson6/Animal.java
+++ src/com/Lesson6/Animal.java
@@ -1,5 +1,40 @@
 package com.Lesson6;
 
 public abstract class Animal {
+    private static int id;
+    private   String name;
+    private  int runingRestirictions;
+    private int swimingRestirictions;
+
+    public static int getId() {
+        return id;
+    }
+
+    public abstract void getVoice();
+
+    protected void setRuningRestirictions(int runingRestirictions) {
+        this.runingRestirictions = runingRestirictions;
+    }
+
+    protected void setSwimingRestirictions(int swimingRestirictions) {
+        this.swimingRestirictions = swimingRestirictions;
+    }
+
+    public Animal(String name) {
+        this.name=name;
+        id++;
+    }
+    public void run(int distance) {
+        writeActivity(distance,runingRestirictions,"ran");
+    }
+
+    public void swim(int distance) {
+        writeActivity(distance,swimingRestirictions,"swam");
+    }
+
+    private void writeActivity(int distance, int restriction, String activity) {
+        if (distance < restriction) System.out.printf("Еру %s is %s %d meters \n",this.name,activity,distance);
+        else System.out.println("This distance to long");
+    }
 
 }
--- src/com/Lesson6/Cat.java
+++ src/com/Lesson6/Cat.java
@@ -1,6 +1,28 @@
 package com.Lesson6;
 
 public class Cat extends Animal {
- 
+    private static int id;
+
+    public Cat(String name) {
+        super(name);
+        setRuningRestirictions(200);
+        setSwimingRestirictions(0);
+        id++;
+    }
+
+    public static int getId() {
+        return id;
+    }
+
+    @Override
+    public void getVoice() {
+        System.out.println("Meou");
+    }
+
+    @Override
+    public void swim(int distance) {
+        System.out.println("Cats can't swim");
+    }
+
 
 }
